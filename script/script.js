7023a935ca611c811d2d02f0b837521e0a175e48
Mohamadap <mohamadmobina11@gmail.com>
2024-03-02 10:53:32 -0800

    add products category and get it from api

--- script/script.js
+++ script/script.js
@@ -18,32 +18,82 @@ $(document).ready(function(){
                 
             });
             $(".product-img").click(function(){
-                var index = $(".product-img").index(this);
-                var product = products[index];
-                var newWindow = window.open('', '_blank');
-            
-                var newHtml =
-                    '<html><head><title>' + product.title + '</title>' +
-                    '<link rel="stylesheet" href="style/bootstrap.rtl.css"/>' +
-                    '</head><body>' +
-                    '<div class="container-fluid">' +
-                    '<div class="row">' +
-                    '<div class="col-lg-4">' +
-                    '<img class="img-fluid" src="' + product.image + '" alt="' + product.title + '">' +
-                    '</div>' +
-                    '<div class="col-lg-8">' +
-                    '<h1>' + product.title + '</h1>' +
-                    '<p>قیمت: ' + product.price + ' تومان</p>' +
-                    '<p>دسته بندی: ' + product.category + '</p>' +
-                    '<p>توضیحات: ' + product.description + '</p>' +
-                    '</div>' +
-                    '</div>' +
-                    '</div>' +
-                    '</body></html>';
-            
-                newWindow.document.write(newHtml);
+                let index = $(".product-img").index(this);
+                let productUrl = 'https://fakestoreapi.com/products/' + (index + 1);
+                $.ajax({
+                    url: productUrl,
+                    type: "GET",
+                    success: function(product){
+                        let newWindow = window.open('', '_blank');
+                        newWindow.document.write(
+                            '<html><head><title>' + product.title + '</title>' +
+                                '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">' +
+                                '</head><body>' +
+                                '<div class="container-fluid">' +
+                                '<div class="row">' +
+                                '<div class="col-lg-4">' +
+                                '<img class="img-fluid" src="' + product.image + '" alt="' + product.title + '">' +
+                                '</div>' +
+                                '<div class="col-lg-4">' +
+                                '<h1>' + product.title + '</h1>' +
+                                '<p>قیمت: ' + product.price + ' تومان</p>' +
+                                '<p>دسته بندی: ' + product.category + '</p>' +
+                                '<p>توضیحات: ' + product.description + '</p>' +
+                                '</div>' +
+                                '</div>' +
+                                '</div>' +
+                                '</body></html>'                
+                                );
+                    },
+                });
             });
             
         }
+    });
+    $.ajax({
+        url: 'https://fakestoreapi.com/products/categories',
+        type: 'GET',
+        success: function(categories) {
+            let $categoriesList = $('#categoriesList');
+            $.each(categories, function(index, category) {
+                $categoriesList.append('<a class="category-item d-flex flex-column " data-category="' + category + '">' + category + '</a>');
+            });
+
+            $('.category-item').click(function() {
+                let category = $(this).data('category');
+                let productUrl = 'https://fakestoreapi.com/products/category/' + category;
+
+                let newWindow = window.open('', '_blank');
+                newWindow.document.write(
+                '<html>'+
+                '<head>'+
+                 '<link rel="stylesheet" href="style/bootstrap.rtl.css" />'+
+                 ' <link rel="stylesheet" href="style/style.css" />'+
+                 '<title>محصولات - ' + category + '</title></head>'+
+                 '<header>'+ product.category+'</header>'+
+                 
+                 '<body>'+
+                 '</body>'+
+                 '</html>'
+                 
+                 );
+
+                 $.ajax({
+                    url: productUrl,
+                    type: 'GET',
+                    success: function(products) {
+                        let productList = '<div class="container-fluid"> <div class="row">'; 
+                        $.each(products, function(index, product) {
+                            productList += '<div class=" col-lg-2 height-category" >';
+                            productList += '<h6>' + product.title + '</h6>';
+                            productList += '<img class="img-category" src="' + product.image + '" alt="' + product.title + '">';
+                            productList +='</div>';
+                        });
+                        productList += '</div> </div>'; 
+                        newWindow.document.body.innerHTML = productList;
+                    },
+                });
+            });
+        },
     });
 });
