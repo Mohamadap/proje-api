54b58521a80ebb49d40d76b95b32a13ba9e069ef
Mohamadap <mohamadmobina11@gmail.com>
2024-03-04 06:32:19 -0800

    saving the latest product category changes in a new JS file

--- script/newPage.js
+++ script/newPage.js
@@ -1,47 +1,59 @@
-$.ajax({
-    url: 'https://fakestoreapi.com/products/categories',
-    type: 'GET',
-    success: function(categories) {
-        let $categoriesList = $('#categoriesList');
-        $.each(categories, function(index, category) {
+$(document).ready(function(){
+
+    $.ajax({
+        url: 'https://fakestoreapi.com/products/categories',
+        type: 'GET',
+        success: function(categories) {
+            let $categoriesList = $('#categoriesList');
+            $.each(categories, function(index, category) {
+                
+                $categoriesList.append('<li><a class="dropdown-item category-item d-flex flex-column " data-category="' + category + '">' + category + '</a></li>');
             
-            $categoriesList.append('<li><a class="dropdown-item category-item d-flex flex-column " data-category="' + category + '">' + category + '</a></li>');
-        
-        });
-
-        $('.category-item').click(function() {
-            let category = $(this).data('category');
-            let productUrl = 'https://fakestoreapi.com/products/category/' + category;
-
-            let newWindow = window.open('', '_blank');
-            newWindow.document.write(
-            '<html>'+
-            '<head>'+
-             '<link rel="stylesheet" href="style/bootstrap.rtl.css" />'+
-             ' <link rel="stylesheet" href="style/style.css" />'+
-             '<title>محصولات - ' + category + '</title></head>'+  
-             '<body>'+
-             
-             '</body>'+
-             '</html>'
-             
-             );
-
-             $.ajax({
-                url: productUrl,
-                type: 'GET',
-                success: function(products) {
-                    let productList = '<div class="container-fluid bg-white"> <div class="row">'; 
-                    $.each(products, function(index, product) {
-                        productList += '<div class=" col-lg-2 height-category" >';
-                        productList += '<h6>' + product.title + '</h6>';
-                        productList += '<img class="img-category" src="' + product.image + '" alt="' + product.title + '">';
-                        productList +='</div>';
-                    });
-                    productList += '</div> </div>'; 
-                    newWindow.document.body.innerHTML = productList;
-                },
             });
-        });
-    },
-});
+
+            $('.category-item').click(function() {
+                let category = $(this).data('category');
+                let productUrl = 'https://fakestoreapi.com/products/category/' + category;
+
+                let newWindow = window.open('', '_blank');
+                newWindow.document.write(
+                '<html>'+
+                '<head>'+
+                 '<link rel="stylesheet" href="style/bootstrap.rtl.css" />'+
+                 ' <link rel="stylesheet" href="style/style.css" />'+
+                 '<title>محصولات - ' + category + '</title></head>'+  
+                 '<body>'+
+                 
+                 '</body>'+
+                 '</html>'
+                 
+                 );
+
+                 $.ajax({
+                    url: productUrl,
+                    type: 'GET',
+                    success: function(products) {
+                        let productList = '<div class="container-fluid bg-white"> <div class="row">'; 
+                        $.each(products, function(index, product) {
+                            productList += '<div class=" col-lg-2 height-category" >';
+                            productList += '<h6>' + product.title + '</h6>';
+                            productList += '<img class="img-category" src="' + product.image + '" alt="' + product.title + '">';
+                            productList +='</div>';
+                        });
+                        productList += '</div> </div>'; 
+                        newWindow.document.body.innerHTML = productList;
+                    },
+                    error: function (error) {
+                        alert("خطا در دریافت اطلاعات محصولات: " + error);
+                      },
+                      complete: function () {
+                        console.log("محصولات به صورت کامل دریافت شد");
+                      },
+                });
+            });
+        },
+    });
+
+
+
+})
